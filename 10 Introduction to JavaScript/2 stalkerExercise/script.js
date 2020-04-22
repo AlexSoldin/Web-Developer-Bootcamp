7583121393a5b8ae35bc8d2a6626f747ffffded6
Alex Soldin <alex.soldin@gmail.com>
2020-04-22 16:13:37 +0200

    Completed Section 10 Introduction to JavaScript

---
+++ 10 Introduction to JavaScript/2 stalkerExercise/script.js
@@ -0,0 +1,6 @@
+var fname = prompt("What is your first name?");
+var sname = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Your full name is " + fname + " " + sname);
+console.log("You are " + age + " years old");
---
+++ 10 Introduction to JavaScript/3 ageCalculator/script.js
@@ -0,0 +1,5 @@
+var age = prompt("How old are you?");
+
+var daysAlive = age * 365.25;
+
+alert(age + " years is roughly " + daysAlive + " days");
