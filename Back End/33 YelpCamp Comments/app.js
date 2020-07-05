94fb96cae3190de370207879fbaef3ee83b49da5
Alex Soldin <alex.soldin@gmail.com>
2020-07-05 18:05:55 +0200

    Completed Section 33 YelpCamp Comments

---
+++ Back End/33 YelpCamp Comments/app.js
@@ -0,0 +1,91 @@
+const campground = require("./models/campground");
+
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    seedDB = require("./seeds");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+seedDB();
+
+app.get("/", (req, res) => {
+    res.render("landing");
+});
+
+app.get("/campgrounds", (req, res) => {
+    Campground.find({}, (err, campgrounds) => {
+        if(err)
+            console.log(err);
+        else
+            res.render("campgrounds/index", {campgrounds: campgrounds});
+    });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", (req, res) => {
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
+    
+    Campground.create(newCampground, (err, newlyCreated) => {
+        if(err)
+            console.log(err);
+        else
+            res.redirect("/campgrounds");
+    });    
+});
+
+app.get("/campgrounds/:id", (req, res) => {
+    Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
+        if(err)
+            console.log(err);
+        else{
+            // console.log(foundCampground);
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err)
+            console.log(err)
+        else
+            res.render("comments/new", {campground: campground});
+    });
+});
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }
+        else{
+            Comment.create(req.body.comment, (err, comment) => {
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+})
